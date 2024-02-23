589a58d404d754d84d339f4fc37742f46c9d301d
halareka <149045655+halareka@users.noreply.github.com>
2024-02-23 19:48:07 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,54 +2,50 @@ console.log('TEST  script');
 function removeDuplicates(arr) {
     return [...new Set(arr)];
 }
-let grid = new Map([
-    [1, [10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19]],
-    [2, [20 , 21 , 22 , 23 , 24 , 25 , 26 , 27 , 28 , 29]],
-    [3, [30 , 31 , 32 , 33 , 34 , 35 , 36 , 37 , 38 , 39]],
-    [4, [40 , 41 , 42 , 43 , 44 , 45 , 46 , 47 , 48 , 49]],
-    [5, [50 , 51 , 52 , 53 , 54 , 55 , 56 , 57 , 58 , 59]],
-    [6, [60 , 61 , 62 , 63 , 64 , 65 , 66 , 67 , 68 , 69]],
-    [7, [70 , 71 , 72 , 73 , 74 , 75 , 76 , 77 , 78 , 79]],
-    [8, [80 , 81 , 82 , 83 , 84 , 85 , 86 , 87 , 88 , 89]],
-    [9, [90 , 91 , 92 , 93 , 94 , 95 , 96 , 97 , 98 , 99]],
-    [10,[100, 101, 102, 103, 104, 105, 106, 107, 108, 109]],
-    [11,[110, 111, 112, 113, 114, 115, 116, 117, 118, 119]],
-    [12,[120, 121, 122, 123, 124, 125, 126, 127, 128, 129]],
-    [13,[130, 131, 132, 133, 134, 135, 136, 137, 138, 139]],
-    [14,[140, 141, 142, 143, 144, 145, 146, 147, 148, 149]],
-    [15,[150, 151, 152, 153, 154, 155, 156, 157, 158, 159]],
-    [16,[160, 161, 162, 163, 164, 165, 166, 167, 168, 169]],
-    [17,[170, 171, 172, 173, 174, 175, 176, 177, 178, 179]],
-    [18,[180, 181, 182, 183, 184, 185, 186, 187, 188, 189]],
-    [19,[190, 191, 192, 193, 194, 195, 196, 197, 198, 199]],
-    [20,[200, 201, 202, 203, 204, 205, 206, 207, 208, 209]],
-
-]);
-//main variables
-const shapes = [14,15,24,25]
-const time = 100;
+function iter_spape_func(){
+    ++count;
+    if(count == 8){count = 1};
+    t_shapes =  shape.get(count);
+    temp_color = color[count];
+    return t_shapes;
+}
+function generateArray(start, rows, cols) {
+    const grid = new Map();
+    let count = start;
+    for (let i = 1; i < rows; i++) {
+        const row = [];
+        for (let j = 0; j < cols; j++) {
+            row.push(count++);
+        }
+        grid.set(i, row);
+    }
+    return grid;
+}
+const grid = generateArray(10, 21, 10);
+const shape = new Map([[1,[14,24,34,44]], [2,[14,24,23,25]],[3,[14,24,34,35]],[4,[14,24,34,33]],[5,[14,15,25,26]],[6,[14,15,24,23]],[7,[14,15,24,25]],]);
+const color = ['transparent','#C9F600','#04859D','#751c5a','#FF9840','#E038AD','#FF4040','#59EA3A','#8243D6'];
+const time = 150;
+let count = 0;
+let t_shapes = [14,15,24,25]
 let intervalId;
-let end = [180,181,182,183,184,185,186,187,188,189];
+let end = [210,211,212,213,214,215,216,217,218,219];
 let colision = [];
-let color = '#cf67c1';
-let color__transparent = 'transparent';
 let temp;
+let temp_color = color[7];
 let temp_arr = [];
-// color func
 
-start_game(shapes)
-
+start_game(t_shapes)
 function start_game(shape){
     get_color(shape);
     function get_color_trp(arr){
         for(let ex = 0;ex < 4;ex++){
-            document.getElementById(arr[ex]).style.background = color__transparent;
+            document.getElementById(arr[ex]).style.background = color[0];
         }
     }
     function get_color(arr){
-        temp = arr; // сохраняем кэш для удаления 
+        temp = arr;
         for(let ex = 0;ex < 4;ex++){
-            document.getElementById(arr[ex]).style.background = color;
+            document.getElementById(arr[ex]).style.background = temp_color;
         }
     }
     function true_down(array){
@@ -68,14 +64,13 @@ function start_game(shape){
                     colision = removeDuplicates(colision.concat(temp))
                     clearInterval(intervalId);
                     grid_cond();
-                    get_color(shape);
+                    get_color(iter_spape_func());
                     startInterval();
                 }
             }
         }, time);
     }
     startInterval();
-
     document.addEventListener('keydown', function(event) {
         if (event.code === 'KeyD') {
             switch_rl(1);                                                                         
@@ -83,9 +78,11 @@ function start_game(shape){
         if (event.code === 'KeyA') {
             switch_rl(-1);                                                                         
         }
+        if (event.code === 'KeyE') {
+            rotate_shape();
+        }
     });
     function switch_rl(i) {
-        console.time('1')
         let temp_switch = [];
         let canMove = true;
         for (let ex = 0; ex < 4; ex++) {
@@ -103,15 +100,15 @@ function start_game(shape){
             get_color_trp(temp);
             get_color(temp_switch);
         }
-        console.timeEnd('1')
-    }  
+    }
+    
 }
 function grid_cond(){
     const t_arr = [];
-    for(let row = 17; row > 4 ; row--){
+    for(let row = 20; row > 4 ; row--){
         let temp_bool = true;
         for (let ex = 0; ex < 10 ;ex++){
-            if(document.getElementById(grid.get(row)[ex]).style.backgroundColor == color__transparent){
+            if(document.getElementById(grid.get(row)[ex]).style.backgroundColor == color[0]){
                 temp_bool = false;
                 break;
             }
@@ -121,46 +118,43 @@ function grid_cond(){
     }
     clear_line(t_arr);
 }
-
 function clear_line(array){
     let b_bd = false;
-    // for(let ex = 0 ; ex < 2;ex++){
     for(let ex = 0 ; ex < array.length;ex++){
         grid.get(array[ex]).forEach((i) => {
-            document.getElementById(i).style.backgroundColor = color__transparent;
+            document.getElementById(i).style.backgroundColor = color[0];
             if(colision.includes(i)){
                 colision.splice(colision.indexOf(i), 1);
                 b_bd = true;
-            }
-            
-        });
+        }});
     }
-    if(b_bd){
-        console.log('линия отчистилас');
-        alert(array)
-        block_down();
-    }
-    // }
+    if(b_bd){line_down();}
 }
-function block_down() {
-    colision.sort((a, b) => b - a);
-    for (let ex = 0; ex < colision.length; ex++) {
-        if (document.getElementById(colision[ex]) != null) {
-            if (colision.includes(colision[ex]+ 10)) {
-                return;
-            } else {
-                document.getElementById(colision[ex]).style.backgroundColor = color__transparent;
+function line_down() {
+    let count = 0;
+    let temp_arr_color = [];
+    let temp_var_math = 200;
+    let temp_switch_lines = [];
+    let temp_colision  = [];
+    let temp_grid = new Map([[1,[]],[2,[]],[3,[]],[4,[]],[5,[]],[6,[]],[7,[]],[8,[]],[9,[]],[10,[]],[11,[]],[12,[]],[13,[]],[14,[]],[15,[]],[16,[]],[17,[]],[18,[]],[19,[]],[20,[]],]);
+    colision.forEach((i)=>{
+        for(let count = 20;count > 1;count--){
+            if(Math.floor(i / 10) == count){
+                temp_grid.get(count).push(i);
+                temp_switch_lines.push(Math.floor(i/10))
+                break;
             }
         }
-    }
-    for (let ex = 0; ex < colision.length; ex++) {
-        colision[ex] += 10;
-        if (document.getElementById(colision[ex]) != null) {
-            if (colision.includes(colision[ex] + 10)) {
-                return;
-            } else {
-                document.getElementById(colision[ex]).style.backgroundColor = color;
-            }
+        temp_arr_color.push(document.getElementById(i).style.backgroundColor)
+        document.getElementById(i).style.backgroundColor = color[0];
+    });
+    const tsl = Array.from(new Set(temp_switch_lines.sort((a,b)=> b - a)))
+    for (let i = 0; i < tsl.length; i++) {
+        for (let ex = 0; ex < temp_grid.get(tsl[i]).length; ex++) {
+            temp_colision.push(temp_grid.get(tsl[i])[ex] + temp_var_math - Math.floor(temp_grid.get(tsl[i])[ex] / 10) * 10); // Corrected collision calculation
         }
+        temp_var_math -= 10;
     }
+    colision = Array.from(new Set(temp_colision));
+    colision.forEach((i)=>{document.getElementById(i).style.backgroundColor = temp_arr_color[count];count++;})
 }
